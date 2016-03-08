1dcae8f736dc2384c6b570b12435ceb1f96e93a6
Paulo Freitas <paulo.freitas@ideais.com.br>
2016-03-07 23:58:40 -0300

    Corrigindo campo Date

--- semana-primefaces/src/main/java/com/algaworks/comercial/model/Orcamento.java
+++ semana-primefaces/src/main/java/com/algaworks/comercial/model/Orcamento.java
@@ -1,5 +1,7 @@
 package com.algaworks.comercial.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -38,5 +40,5 @@ public class Orcamento {
 	
 	@Temporal(TemporalType.DATE)
 	@Column(name="data_validade")
-	private String dataValidade;
+	private Date dataValidade;
 }
